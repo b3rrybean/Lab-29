363c596d167094a06ce7b19f4841d75b7f10aef7
Nicole C. <nmcanson@gmail.com>
2025-11-07 02:11:21 -0800

    Eighth commit

--- Lab-29.cpp
+++ Lab-29.cpp
@@ -14,6 +14,25 @@
     // [2] = casual fans
     // [3] = new fans
 
-// Open an external file called fan.stxt to read initial fan data
+// Open an external file called fans.txt to read initial fan data
     // If the file doesn't open, print an error message and exit the program
-// Read data from file and populate map
+// Read data from file and populate map by extracting opponent name, fan type, and fan ID
+// Insert the fan ID into the correct list in the array for that opponent
+// Might need to fix how commas are handled, depends how data file is formatted
+
+// Close the file
+
+// Begin a time-based simulation for attendance changes
+    // For 25 minute intervals
+        // Iterate through random attendance events:
+            // Sometimes add a few new fans
+            // Sometimes remove a few existing ones
+            // Sometimes convert a first-time fan into a casual fan
+    // Print what happened this time period, e.g. " "
+
+
+// End of function
+
+// I think the simulation function should go up top? not sure yet...
+
+// End of program
