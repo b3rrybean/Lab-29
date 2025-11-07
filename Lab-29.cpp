525990a4ef08f67bf59d7036fc091c48037c82ed
Nicole C. <nmcanson@gmail.com>
2025-11-07 01:40:58 -0800

    Fifth commit

---
+++ Lab-29.cpp
@@ -0,0 +1,12 @@
+// Include necessary headers
+
+// Define constants
+
+// Define function to simulate attedance changes overtime
+// Parameters:
+
+// Define main function:
+// Initialize a map to store hockey game information
+// The key is the opponent team (like "Ducks", "Kings", etc.)
+
+// not sure where everything should go quite yet... thinking ..
