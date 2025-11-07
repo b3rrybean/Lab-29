89d97b9a4fa6d4331ac335f966f595dc42e2d31c
Nicole C. <nmcanson@gmail.com>
2025-11-07 02:31:28 -0800

    Tenth commit

--- Lab-29.cpp
+++ Lab-29.cpp
@@ -3,28 +3,9 @@
 // Define constants
 
 // Define function to simulate attedance changes overtime
-    // simulateAttendanceChanges(map, periods)
-    // Parameters: map of opponent teams, number of games (time periods)
-
-
-// Define main function:
-    // Initialize a map to store hockey game information
-        // Each opponent name (like "Ducks", "Kings", etc.) will be the map key
-        // The value will be an array of three lists:
-            // [1] = season ticket holders
-            // [2] = casual fans
-            // [3] = new fans
-
-// Open an external file called fans.txt to read initial fan data
-    // If the file doesn't open, print an error message and exit the program
-// Read data from file and populate map by extracting opponent name, fan type, and fan ID
-    // Insert the fan ID into the correct list (season, casual, or first-time)
-        // Read each line from the file using getline
-        // Split the line at commas into opponent, fan type, fan ID
-
-// Close the file
-
-// Begin a time-based simulation for attendance changes
+// simulateAttendanceChanges(map, periods)
+// Parameters: map of opponent teams, number of games (time periods)
+    // Begin a time-based simulation for attendance changes
     // For 25 minute intervals
         // Iterate through random attendance events:
             // Sometimes add a few new fans
@@ -37,6 +18,28 @@
 
 // End of function
 
-// I think the simulation function should go up top? not sure yet...
+// Define main function:
+    // Initialize a map to store hockey game information
+        // Each opponent name (like "Ducks", "Kings", etc.) will be the map key
+        // The value will be an array of three lists:
+            // [1] = season ticket holders
+            // [2] = casual fans
+            // [3] = new fans
+
+    // Open an external file called fans.txt to read initial fan data
+        // If the file doesn't open, print an error message and exit the program
+    // Read data from file and populate map by extracting opponent name, fan type, and fan ID
+        // Insert the fan ID into the correct list (season, casual, or first-time)
+            // Read each line from the file using getline
+            // Split the line at commas into opponent, fan type, fan ID
+    
+    // Close the file
+
+    // Display the initial state of attendance
+        // For each opponent, print how many fans are in each list
+    
+    // Call simulateAttendanceChanges with the map and 25 time periods
+
+// End of main function
 
 // End of program
